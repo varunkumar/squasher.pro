a004efad274bd9b5fc114613012ff1be09c4ffba
Varunkumar Nagarajan <varunkumar.n@gmail.com>
2021-12-04 10:59:00 +0530

    Initial version with popup, API calls and logos

--- chrome-extension/src/pages/Content/index.js
+++ chrome-extension/src/pages/Content/index.js
@@ -1,6 +1,113 @@
-import { printLine } from './modules/print';
-
 console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
+class Extractor {
+  constructor() {}
 
-printLine("Using the 'printLine' function from the Print Module");
+  getPageData() {}
+
+  isPageDetected() {}
+}
+
+class AllPages extends Extractor {
+  constructor() {
+    super();
+  }
+
+  getPageData() {
+    let content = document.body.innerText;
+    if (content.length > 5000) {
+      // use only first 5000 characters
+      content = content.substring(0, 5000);
+    }
+    return content;
+  }
+
+  isPageDetected() {
+    return true;
+  }
+}
+
+class WikiExtractor extends Extractor {
+  constructor() {
+    super();
+  }
+
+  getPageData() {
+    let content = document.querySelector('#main-content').innerText;
+    let title = document.querySelector(
+      '#main #title-heading #title-text'
+    ).innerText;
+    return `${title}\n${content}`;
+  }
+
+  isPageDetected() {
+    const url = window.location.href;
+    if (url.includes('wiki')) {
+      let content = document.querySelector('#main-content');
+      if (content) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+class TicketExtractor extends Extractor {
+  constructor() {
+    super();
+  }
+
+  getPageData() {
+    let container = document.querySelector('#updates_container');
+    let updates = container.querySelectorAll('.request_item');
+
+    // Iterate through updates nodelist
+    const extractedContents = [...updates].map((update) => {
+      let id = update.getAttribute('data-updateid');
+      let user = update
+        .querySelector('.request_head .user-details .user-mouseover')
+        .getAttribute('data-user');
+      let content = update.querySelector(
+        '.update_container .request_desc'
+      ).innerText;
+      let timestamp = update
+        .querySelector('.request_head .upd-time')
+        .getAttribute('data-timestamp');
+      return { id, user, content, timestamp };
+    });
+
+    // Sort extractedContents by id
+    const sortedContents = extractedContents.sort((a, b) => {
+      return a.id - b.id;
+    });
+
+    let content = '';
+    sortedContents.forEach((update) => {
+      if (update.content && update.content.length > 0) {
+        content += `${update.user}: ${update.content}\n`;
+      }
+    });
+    return content;
+  }
+
+  isPageDetected() {
+    const url = window.location.href;
+    if (url.includes('desflow')) {
+      let content = document.querySelector('#updates_container');
+      if (content) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+const extractors = [new WikiExtractor(), new TicketExtractor(), new AllPages()];
+chrome.runtime.onMessage.addListener((event, sender, sendResponse) => {
+  if (event.type === 'get-page-data') {
+    extractors.forEach((extractor) => {
+      if (extractor.isPageDetected()) {
+        sendResponse(extractor.getPageData());
+      }
+    });
+  }
+});
--- chrome-extension/src/pages/Popup/Popup.jsx
+++ chrome-extension/src/pages/Popup/Popup.jsx
@@ -1,27 +1,140 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { Component, Fragment } from 'react';
+import reload from '../../assets/img/dislike.png';
+import heart from '../../assets/img/like.png';
+import loading from '../../assets/img/loading.gif';
+import logo from '../../assets/img/logo.png';
 import './Popup.css';
 
-const Popup = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+class Popup extends Component {
+  state = {
+    loading: true,
+    feedback: false,
+  };
+
+  componentDidMount() {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      chrome.tabs.sendMessage(tabs[0].id, {type: 'get-page-data'}, async content => {
+        console.log(content);
+        this.setState({content});
+        let formData = new FormData();
+        formData.append("content", content);
+        try {
+          const response = await fetch('https://api.squasher.pro/squashit', {
+            method: 'POST',
+            body: formData
+          });
+          const data = await response.text();
+          if (data.length > 0) {
+            this.setState({ 
+              summary: response.status === 200 ? data : 'Unable to summarize the page',
+              error: response.status !== 200 ? 'error' : '',
+              loading: false,
+              feedback: false,
+              engine: 'openai'
+            });
+          } else {
+            this.setState({ 
+              summary: 'Unable to summarize the page',
+              error: 'error',
+              loading: false,
+              feedback: false
+            });
+          }
+        } catch (error) {
+          this.setState({ 
+            summary: 'Unable to summarize the page',
+            error: 'error',
+            loading: false,
+            feedback: false
+          });
+        }
+      });
+    });
+  }
+
+  refresh = () => {
+    this.setState({loading: true, summary: null, error: null});
+    this.componentDidMount();
+  }
+
+  accept = () => {
+    this.sendFeedback(this.state.content, this.state.summary, this.state.summary, this.state.engine);
+  }
+
+  toggleFeedback = () => {
+    this.setState({feedback: !this.state.feedback});
+  }
+
+  handleTextChange = (event) => {
+    this.setState({ suggested: event.target.value });
+  }
+
+  submitFeedback = () => {
+    this.setState({loadingFeedback: true, feedback: true});
+    this.sendFeedback(this.state.content, this.state.summary, this.state.suggested, this.state.engine);
+    this.setState({loadingFeedback: false, feedback: false});
+  }
+
+  sendFeedback = async (content, summary, suggestion, engine) => {
+    let formData = new FormData();
+    formData.append("content", content);
+    formData.append("original_summary", summary);
+    formData.append("suggested_summary", suggestion);
+    formData.append("engine", engine);
+    try {
+      const response = await fetch('https://api.squasher.pro/feedback', {
+        method: 'POST',
+        body: formData
+      });
+      await response.text();
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {this.state.loading ? (
+            <div className="loader">
+              <img src={loading} className="App-logo" style={{width: 300, height: 400}} alt="loading" />
+            </div>
+          ) : <img src={logo} className="App-logo" alt="logo" />}
+        </header>
+        {this.state && this.state.summary && (
+          <Fragment>
+            {this.state.engine && (
+                <div className="App-engine">
+                  <span>Engine: </span>
+                  <b>{this.state.engine}</b>
+                </div>
+              )}
+            <div className={`App-content ${this.state.error}`}>
+              {this.state.summary}
+              <div className="App-content-overlay">
+                <img src={reload} className="action" onClick={this.refresh}/>
+                <img src={heart} className="action" onClick={this.accept}/>
+              </div>
+            </div>
+            <div className="App-footer">
+              <a href="#123" className="App-link" onClick={this.toggleFeedback}>Help improve!</a>
+            </div>
+            {this.state.feedback && (
+              <div className="App-feedback">
+                <textarea placeholder="Feedback" onChange={this.handleTextChange}>
+                {this.state.summary}
+                  </textarea>
+                {this.state.loadingFeedback ? "Submitting...  ": ""}
+                <button onClick={this.submitFeedback}>Submit</button>
+              </div>
+            )}
+          </Fragment>
+          )}
     </div>
-  );
-};
+    );
+  }
+}
 
 export default Popup;
